b9677a74da410685980de8f8a7bd8c2b466d1a18
prestonchso <prestonchso@gmail.com>
2021-03-05 16:45:08 -0800

    First Draft

--- StarValidation.py
+++ StarValidation.py
@@ -1,1 +1,12 @@
 #Starting
+start = 1
+end = 5
+rating = float(input(f"What will be your rating today ({start}-{end}): "))
+
+def validate(rating,start,end):
+    while rating > end or rating < start:      
+        print("That is not within the range. Try again")
+        rating = float(input(f"What will be your rating today ({start}-{end}): "))
+        
+    print("Thank you for your input. We hope to see you again")
+validate(rating,start,end)
