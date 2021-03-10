6b5bca56a98b4490379b618cfe9edc898eab9571
prestonchso <prestonchso@gmail.com>
2021-03-10 14:31:58 -0800

    Fixed "Not Defined error"

---
+++ Triangle Trig/TriangleTrig.py
@@ -0,0 +1,5 @@
+#https://www.101computing.net/triangle-geometry-functions/
+#import triangleFunctions
+from triangleFunctions import *
+#Must save other files first before calling other lines of code
+getPerimeter(2,3,4)
---
+++ Triangle Trig/triangleFunctions.py
@@ -0,0 +1,4 @@
+def getPerimeter(a,b,c):
+    perimeter = a+b+c
+    print("The perimeter of a triangle with sidelengths: " + str(a) + ", "+ str(b) + ", "+ str(c) + ", will have a perimeter of:", perimeter)
+lst = ["preston", "jason"]
