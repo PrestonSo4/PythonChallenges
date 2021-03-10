45d4fbfaa49be066babf37f9fe9ac4f9373a8256
prestonchso <prestonchso@gmail.com>
2021-03-10 14:54:06 -0800

    Testing PythagTheorem

--- Triangle Trig/triangleFunctions.py
+++ Triangle Trig/triangleFunctions.py
@@ -1,3 +1,4 @@
+from math import sqrt
 def getPerimeter(a,b,c):
     perimeter = a+b+c
     print("The perimeter of a triangle with sidelengths: " + str(a) + ", "+ str(b) + ", "+ str(c) + ", will have a perimeter of:", perimeter)
@@ -20,3 +21,11 @@ def isIsoscles(a,b,c):
         print("A triangle with the sideslengths of:",str(a) + ",",str(b) + ",",str(c) + ", is an Isoscles triangle.")
     else:
         print("Your triangle with the specified information is not an isoscles tirangle")        
+def pythagTheorem():
+    q = input("Are you solving for c (the hypotenuse)?(y/n) ").lower()
+    if q == "y":
+        a = float(input("What is the value for sidelength a? "))
+        b = float(input("What is the value for sidelength b? "))
+        c = sqrt(a**2+b**2)
+        print("The length of side C is:", c)
+
